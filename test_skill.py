6cdd47dfc2caa6f56577d529f18c6d068bc4e334
Jean-Michel Lekston <lekston.jm@gmail.com>
2019-09-18 14:16:43 +0200

    Update test_skill.py

--- test_skill.py
+++ test_skill.py
@@ -10,16 +10,6 @@ agent = create_skill_agent(os.path.dirname(__file__), lang='en')
 class TestTemplateSkill:
 
   def setup(self):
-    # Between each tests, resets the model mock so calls are dismissed and we
-    # start on a fresh state.
-    # This will be usefull when you have more than one test method :)
-    agent.model.reset()
-
-  def test_it_should_launch_the_skill(self):
-    # Now, try to trigger our skill
-    agent.parse('launch template skill')
-
-    # And make assertions about how the model (the part between pytlas and the end user)
-    # as answered: https://pytlas.readthedocs.io/en/latest/writing_skills/testing.html#writing-tests
-    call = agent.model.on_answer.get_call()
-    expect(call.text).to.equal('Hello from the template skill')
+    #TODO
+    pass
+    
